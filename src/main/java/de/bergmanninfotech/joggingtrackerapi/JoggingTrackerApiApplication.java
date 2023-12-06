d11ce51ec7d5fe4a1f063e92c216b6640c2db18f
belov <andrey.beloborodov.job@gmail.com>
2023-12-06 21:35:03 +0400

    init commit: skeleton from spring initializr

---
+++ src/main/java/de/bergmanninfotech/joggingtrackerapi/JoggingTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package de.bergmanninfotech.joggingtrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoggingTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoggingTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/bergmanninfotech/joggingtrackerapi/JoggingTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bergmanninfotech.joggingtrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoggingTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
